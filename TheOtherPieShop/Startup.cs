3747881eaa080d420db3a0a7230894b386441b40
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-01-13 18:34:23 +0100

    Added Some Models

--- TheOtherPieShop/Startup.cs
+++ TheOtherPieShop/Startup.cs
@@ -15,6 +15,7 @@ namespace TheOtherPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +25,9 @@ namespace TheOtherPieShop
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
