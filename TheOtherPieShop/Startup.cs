333fee06cd7fd66588d4247009f3dff59ac28222
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-01-21 12:59:55 +0100

    Switching to sqldb, not created db yet

--- TheOtherPieShop/Startup.cs
+++ TheOtherPieShop/Startup.cs
@@ -2,21 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheOtherPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using TheOtherPieShop.Models;
 
 namespace TheOtherPieShop
 {
     public class Startup
+
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddTransient<IPieRepository, PieRepository>();
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 
